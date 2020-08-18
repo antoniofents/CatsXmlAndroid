e599caca6119e95d2ff3d27a3d9768a032943b0a
antoniofents <antoniofentanesg@gmail.com>
2020-08-18 08:57:36 -0500

    Update ExampleUnitTest.java (POEditor.com)

--- app/src/test/java/com/example/afentanes/customlists/ExampleUnitTest.java
+++ app/src/test/java/com/example/afentanes/customlists/ExampleUnitTest.java
@@ -1,17 +1,219 @@
-package com.example.afentanes.customlists;
-
-import org.junit.Test;
-
-import static org.junit.Assert.*;
-
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
-public class ExampleUnitTest {
-    @Test
-    public void addition_isCorrect() throws Exception {
-        assertEquals(4, 2 + 2);
+{
+    "common": {
+        "ok": "OK",
+        "cancel": "Cancel",
+        "back": "Back",
+        "packageId": "Package id",
+        "orderId": "Order id",
+        "retailer": "Retailer",
+        "spaceRoute": "Space route",
+        "route": "Route",
+        "package": "Package",
+        "address": "Address",
+        "retry": "Retry",
+        "continue": "",
+        "fastScanError": {
+            "message": ""
+        }
+    },
+    "apiErrors": {
+        "unknown": "Unknown error",
+        "unknownMessage": "An unknown error happened",
+        "badParameters": "Bad parameters error",
+        "badParametersMessage": "The parameters sent are wrong",
+        "badData": "Bad data error",
+        "badDataMessage": "The received data is unexpected",
+        "serverError": "Server error",
+        "serverErrorMessage": "Unexpected server error",
+        "packageNotFound": "Package not found",
+        "packageNotFoundMessage": "The package is not declared in Paack's systems",
+        "spaceNotFound": "Space not found",
+        "spaceNotFoundMessage": "The space is not declared in Paack's systems",
+        "networkConnection": "",
+        "networkConnectionMessage": ""
+    },
+    "cmp": {
+        "BleScanner": {
+            "onDisconnectError": "Please, turn it on again",
+            "onDisconnectManager": "Device's bluetooth was turned off",
+            "onDisconnectTitle": "Barcode scanner disconnected",
+            "onErrorTitle": "Barcode scanner disconnected",
+            "onStartMessage": "Please, turn on the scanner",
+            "onStartTitle": "Looking for the barcode scanner"
+        },
+        "appVersion": {
+            "prefix": ""
+        },
+        "backendSelector": {
+            "button": "",
+            "title": ""
+        },
+        "bleScanner": {
+            "bluetoothDisconnected": "",
+            "enableBluetooth": "",
+            "enableLocation": "",
+            "errorTitle": "",
+            "locationDisconnected": "",
+            "scannerDisconnected": "",
+            "searchScanner": "",
+            "retry": "",
+            "useCamera": ""
+        },
+        "packageDetails": {
+            "currentSpace": "",
+            "deliveryDate": "",
+            "emptyHistory": "",
+            "itemSubtitle": "",
+            "lastStatusTime": "",
+            "notPaired": "",
+            "orderNumber": "",
+            "package": "",
+            "packageHistory": "",
+            "packageNumber": "",
+            "retailer": "",
+            "route": "",
+            "routeSequence": "",
+            "title": "",
+            "timeslot": {
+                "title": "",
+                "subtitle": ""
+            }
+        }
+    },
+    "homeScreen": {
+        "welcome": "Hello!",
+        "welcomeDescription": "What do you want to do today?",
+        "options": {
+            "labeling": "Labeling",
+            "labelingDescription": "Package label printing process",
+            "sorting": "Move a package",
+            "sortingDescription": "Package sorting process",
+            "dispatching": "Dispatch a round",
+            "dispatchingDescription": "Package dispatching process",
+            "returnsReception": "Returns reception",
+            "returnsReceptionDescription": "Reception package return process",
+            "returnsDispatching": "Returns dispatching",
+            "returnsDispatchingDescription": "Dispatching package return process",
+            "packageManagement": "Package management",
+            "packageManagementDescription": "Package management and info",
+            "settings": "Settings",
+            "settingsDescription": "Configure your device",
+            "packageDetails": "",
+            "logout": ""
+        }
+    },
+    "settingsScreen": {
+        "scannerMode": {
+            "label": "Scanner mode",
+            "offMode": "Camera",
+            "onMode": "Bluetooth"
+        }
+    },
+    "storybook": {
+        "loremIpsum": "Lorem ipsum",
+        "placeholder": "Placeholder",
+        "field": "Field"
+    },
+    "loginForm": {
+        "fieldLabelUsername": "Username",
+        "fieldLabelPassword": "Password",
+        "placeholderUsername": "Enter your username",
+        "placeholderPassword": "Enter your password",
+        "button": "Login",
+        "usernameRequired": "Username required",
+        "passswordRequired": "Password required",
+        "feedbackLogin": "Logging in...",
+        "feedbackError": "Invalid username or password",
+        "passwordRequired": "",
+        "buttonSubmit": ""
+    },
+    "sortingScreen": {
+        "package": {
+            "scan": {
+                "title": "Scan now",
+                "message": "Scan an item to add it to a space",
+                "button": "Back"
+            },
+            "success": {
+                "title": "Package found!",
+                "message": "Package scanned correctly",
+                "button": "Continue"
+            },
+            "error": {
+                "title": "Oops!",
+                "message": "Invalid package barcode",
+                "button": "Back"
+            }
+        },
+        "space": {
+            "scan": {
+                "title": "Scan a space",
+                "message": "Scan the space to add the package\n{{barcode}}",
+                "button": "Back"
+            },
+            "success": {
+                "title": "Done",
+                "message": "Item added to space",
+                "button": "Continue"
+            },
+            "error": {
+                "title": "Oops!",
+                "button": "Back",
+                "message": "Invalid space barcode"
+            }
+        }
+    },
+    "dispatchScreen": {
+        "space": {
+            "scan": {
+                "title": "Scan a space",
+                "message": "Scan a space to detach the packages",
+                "backButton": "Back"
+            },
+            "success": {
+                "title": "Done",
+                "message": "Dispatch the space",
+                "button": "Continue"
+            },
+            "error": {
+                "title": "Oops!",
+                "message": "Looks like something went wrong...",
+                "button": "Back"
+            }
+        },
+        "confirm": {
+            "success": {
+                "title": "Done",
+                "message": "Packages delivered",
+                "button": "Continue"
+            }
+        },
+        "confirmation": {
+            "scan": {
+                "title": "",
+                "message": ""
+            }
+        }
+    },
+    "scan-driver-confirmation": {
+        "title": "Please hand over the packages",
+        "message": "Scan the barcode to confirm",
+        "backButton": "Back"
+    },
+    "searchPackageScreen": {
+        "scan": {
+            "title": "",
+            "message": "",
+            "button": ""
+        },
+        "success": {
+            "message": ""
+        },
+        "error": {
+            "title": ""
+        }
+    },
+    "cameraMask": {
+        "flashlight": ""
     }
 }
